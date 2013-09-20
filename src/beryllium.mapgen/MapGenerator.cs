eaeec42ab1d2cbfdc7b38b54cf63b1b6a973076d
cyranothedaft <cyranothedev@gmail.com>
2013-09-20 17:22:29 -0600

    map generator started

---
+++ src/beryllium.mapgen/MapGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using beryllium.lib;
+using beryllium.lib.Model;
+
+
+
+namespace beryllium.mapgen {
+   public sealed class MapGenerator : IWorldProcessor {
+      private readonly string _outputDir;
+
+
+      public MapGenerator(string outputDir) {
+         _outputDir = outputDir;
+      }
+
+
+      public void ProcessLevelDirectory(LevelDirectoryMetadata levelDirectoryMetadata) {
+         //
+      }
+
+
+      public void ProcessLevelMetadata(LevelMetadata levelMetadata) {
+         //
+      }
+
+
+      public void ProcessDimensionMetadata(DimensionMetadata dimension) {
+         //
+      }
+
+
+      public void ProcessRegionHeader(Region region) {
+         //
+      }
+
+
+      public void ProcessChunk(Chunk chunk) {
+         //
+      }
+
+
+      public void ProcessRegionEnd(Region region) {
+         //
+      }
+
+
+      public void ProcessDimensionEnd(DimensionMetadata dimension) {
+         //
+      }
+
+
+      public void ProcessLevelEnd(LevelMetadata levelMetadata) {
+         //
+      }
+   }
+}
